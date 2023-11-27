41104aafc14fd769045caf19ebde5320b7e0a7ef
Guilherme Nunes <123277481+Guisnu@users.noreply.github.com>
2023-11-27 17:26:56 -0300

    fix: layout

--- main.js
+++ main.js
@@ -1,14 +1,22 @@
 document.getElementById("toggle").addEventListener("click", toggle);
+document.getElementById("pitch").addEventListener("click", pitch);
 
 function toggle(e) {
 	const content = document.getElementById("content");
 
 	if (content.style.getPropertyValue("display") == "none") {
 		content.style.setProperty("display", "block");
-		e.target.innerText = "Esconder";
 	} else {
 		content.style.setProperty("display", "none");
-		e.target.innerText = "Veja mais";
+	} 
+}
+function pitch(e) {
+	const content = document.getElementById("banner");
+
+	if (content.style.getPropertyValue("display") == "none") {
+		content.style.setProperty("display", "flex");
+	} else {
+		content.style.setProperty("display", "none");
 	} 
 }
 
