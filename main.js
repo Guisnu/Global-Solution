1b88cd4be5b36f5d32a87c142f9b30e8e41510c1
tiagoinaba <tiago.inaba@gmail.com>
2023-11-30 22:22:29 -0300

    merged

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 document.getElementById("toggle").addEventListener("click", toggle);
 document.getElementById("pitch").addEventListener("click", pitch);
+document.getElementById("newsletterForm").addEventListener("submit", handleSubmit);
 
 function toggle(e) {
 	const content = document.getElementById("content");
@@ -20,3 +21,12 @@ function pitch(e) {
 	} 
 }
 
+function handleSubmit(e) {
+    e.preventDefault();
+    
+    const nome = document.getElementById("nome").value;
+    const email = document.getElementById("email").value;
+    console.log(email);
+
+    alert(`Obrigado, ${nome}! Te manteremos atualizado sobre a Helfhiap.`);
+}
